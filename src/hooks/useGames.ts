d70e3344259ec452f36d9fd0c5a5b85a8a570f2d
Layla Le <info.aeslilly@gmail.com>
2025-03-26 17:14:42 -0400

    built loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,15 +6,17 @@ export interface Game {
     id: number;
     name: string;
     background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
+
 export interface Platform {
     id: number;
     name: string;
     slug: string;
 }
-interface FetchgamesResponse {
+
+interface FetchGamesResponse {
     count: number;
     results: Game[];
 }
@@ -22,21 +24,28 @@ interface FetchgamesResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState('');
+    const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
         const controller = new AbortController();
-
-        apiClient.get<FetchgamesResponse>('/games', { signal: controller.signal })
-            .then(res => setGames(res.data.results))
+        
+        setLoading(true);
+        
+        apiClient.get<FetchGamesResponse>('/games', { signal: controller.signal })
+            .then(res => {
+                setGames(res.data.results);
+                setLoading(false);
+            })
             .catch(err => {
-                if(err instanceof CanceledError) return;
-                else
-                setError(err.message)});
+                if (err instanceof CanceledError) return;
+                setError(err.message);
+                setLoading(false);
+            });
 
-            return () => controller.abort();
-    }, [])
+        return () => controller.abort();
+    }, []);
 
-    return { games, error };
-}
+    return { games, error, isLoading };
+};
 
 export default useGames;
