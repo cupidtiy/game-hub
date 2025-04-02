4bd1bbe4b3d49c640dc39708eeebf9272e5f8080
Layla Le <info.aeslilly@gmail.com>
2025-04-01 21:57:30 -0400

    display genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import { Grid, GridItem, Show, Text } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
 import GameGrid from "./components/GameGrid"
+import GenreList from "./components/GenreList"
 
 function App() {
   return (
     <Grid templateAreas={{
       base: '"nav" "main"',
-      lg: '"nav nav" "main aside"'
-    }}>
+      lg: '"nav nav" "aside main"'
+    }}
+    templateColumns={{
+      base: '1fr',
+      lg: '200px 1fr'
+    }}
+    >
 
       <GridItem area='nav' >
         <NavBar>
         </NavBar>
       </GridItem>
 
-      <Show above="lg"><GridItem area='aside' >aside</GridItem></Show>
+      <Show above="lg">
+        <GridItem area='aside' paddingX={5}>
+        <GenreList />
+        </GridItem></Show>
       <GridItem area='main' >
         <GameGrid />
       </GridItem>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,27 @@
+import { HStack, List, ListItem, Image, Text } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-    const {data} = useGenres();
+    const { data } = useGenres();
 
-  return (
-    <div>GenreList</div>
-  )
+    return (
+        <List>
+            {data.map(genre => <ListItem key={genre.id} 
+            paddingY = '5px'
+            fontSize = 'lg'>
+                <HStack>
+                    <Image boxSize = '32px' 
+                    borderRadius={8} 
+                    src = {getCroppedImageUrl(genre.image_background)}>
+                    </Image>
+                    <Text>
+                        {genre.name}
+                    </Text>
+            </HStack>
+            </ListItem>)}
+        </List>
+    )
 }
 
 export default GenreList
