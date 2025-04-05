c758232706638425ff6cb9b1f0d3603d71baf4d8
Layla Le <info.aeslilly@gmail.com>
2025-04-05 17:43:37 -0400

    refactoring - create a reusable api client

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,32 +1,31 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient, { FetchResponse } from "../services/api-client";
+import { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 import { GameQuery } from "../App";
 import { Platform } from "./usePlatforms";
 
 export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
+    rating_top: number;
 }
 
+const apiClient = new APIClient<Game>('/games');
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
-    queryKey: ['games', gameQuery],
-    queryFn: () =>
-      apiClient
-        .get<FetchResponse<Game>>('/games', {
-          params: {
-            genres: gameQuery.genre?.id,
-            parent_platforms: gameQuery.platform?.id,
-            ordering: gameQuery.sortOrder,
-            search: gameQuery.searchText,
-          },
+    useQuery<FetchResponse<Game>, Error>({
+        queryKey: ['games', gameQuery],
+        queryFn: () => apiClient.getAll({
+            params: {
+                genres: gameQuery.genre?.id,
+                parent_platforms: gameQuery.platform?.id,
+                ordering: gameQuery.sortOrder,
+                search: gameQuery.searchText,
+            },
         })
-        .then((res) => res.data),
-  });
+    });
 
 export default useGames;
