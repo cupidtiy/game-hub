a354564790235d2f381df5c1c17b446237c64a0a
Layla Le <info.aeslilly@gmail.com>
2025-04-08 12:42:01 -0400

    refactor - create lookup hooks

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -3,19 +3,20 @@ import { q } from 'framer-motion/dist/types.d-B50aGbjN'
 import React from 'react'
 import { GameQuery } from '../App'
 import useGenres from '../hooks/useGenres'
+import usePlatform from '../hooks/usePlatform'
+import useGenre from '../hooks/useGenre'
 
 
 interface Props {
     gameQuery: GameQuery
 }
 const GameHeading = ({ gameQuery }: Props) => {
-    const { data: genres } = useGenres();
-    const genre = genres?.results.find(g => g.id === gameQuery.genreId);
+   
+const platform = usePlatform(gameQuery.platformId);
 
-    const { data: platforms } = useGenres();
-    const platform = platforms?.results.find(g => g.id === gameQuery.platformId);
+const genre = useGenre(gameQuery.genreId);
 
-    const heading = `${platform?.name || ''} ${genre?.name || ''} Games`
+    const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
     return (
         <Heading as='h1' marginY={5} fontSize={'5xl'}>
